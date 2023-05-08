b57160dcb90e4f7c88e1ad1e9f02dde798a0062b
kim-museong <antjd0419@gmail.com>
2023-05-08 10:03:20 +0900

    수정

--- controllers/page.js
+++ controllers/page.js
@@ -460,7 +460,7 @@ exports.rowViewList = async (req, res, next) => {
 exports.newestList = async (req, res, next) => {
   try {
     const viewlist = await Post.findAll({
-      order: [["createdAt", "DESC"]],
+      order: [["id", "DESC"]],
       include: [
         {
           model: User,
@@ -478,7 +478,7 @@ exports.newestList = async (req, res, next) => {
 exports.oldList = async (req, res, next) => {
   try {
     const viewlist = await Post.findAll({
-      order: [["createdAt"]],
+      order: [["id"]],
       include: [
         {
           model: User,
